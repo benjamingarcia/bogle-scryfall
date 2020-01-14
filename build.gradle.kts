fc37d3e785e81c509c66c3e2ea6fda08844e24f2
Benjamin Garcia <bgarcia@zendesk.com>
2020-01-14 11:53:02 +0100

    ✨ : initialize akka-http server

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,8 @@ repositories {
 dependencies {
     implementation("ch.qos.logback", "logback-classic", "1.2.3")
     implementation("org.scala-lang", "scala-library", "2.12.9")
-    implementation("com.typesafe.akka", "akka-http_2.12", "10.1.10")
-    implementation("com.typesafe.akka", "akka-stream_2.12", "2.5.26")
-    testImplementation("org.scalatest", "scalatest_2.12", "3.0.8")
+    implementation("com.typesafe.akka", "akka-http_2.13", "10.1.11")
+    implementation("com.typesafe.akka", "akka-stream_2.13", "2.6.1")
+    testImplementation("org.scalatest", "scalatest_2.13", "3.0.8")
     testImplementation("junit", "junit", "4.12")
-
 }
--- src/main/scala/org/bogle/Bogle.scala
+++ src/main/scala/org/bogle/Bogle.scala
@@ -1,30 +1,43 @@
 package org.bogle
 
-import org.slf4j.LoggerFactory
-import org.slf4j.Logger
-import com.typesafe.config.ConfigFactory
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
+import com.typesafe.config.ConfigFactory
+import org.slf4j.{Logger, LoggerFactory}
 
-import scala.concurrent.Future
-import scala.util.{ Failure, Success }
+import scala.concurrent.{Await, Future}
 
 object Bogle extends App {
   val SCRYFALL_KEY = "scryfall.key"
   val TELEGRAM_TOKEN = "telegram.token"
 
-  val LOG:Logger = LoggerFactory.getLogger(Bogle.getClass())
+  val LOG: Logger = LoggerFactory.getLogger(Bogle.getClass())
   val config = ConfigFactory.load()
   LOG.info("Let's start Bogle to be Hexproof")
-  LOG.info(s"${SCRYFALL_KEY} => ${config.getString(SCRYFALL_KEY)}") 
+  LOG.info(s"${SCRYFALL_KEY} => ${config.getString(SCRYFALL_KEY)}")
   LOG.info(s"${TELEGRAM_TOKEN} => ${config.getString(TELEGRAM_TOKEN)}")
 
 
   implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
-}
 
+  val route =
+    path("card") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+    Http().bind(interface = "localhost", port = 8080)
+  val bindingFuture: Future[Http.ServerBinding] =
+    serverSource.to(Sink.foreach { connection => // foreach materializes the source
+      println("Accepted new connection from " + connection.remoteAddress)
+    }).run()
+
+}
